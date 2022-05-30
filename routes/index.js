331be8709ecd9c28ae983a1dd71a9083f3644ece
ErwanGlmt <erwanguillemot7@gmail.com>
2022-05-30 12:05:33 +0200

    Fin de la quête 10

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const moviesRouter = require('./movies');
 const usersRouter = require('./users');
+const authRouter = require('./auth')
 
 const setupRoutes = (app) => {
   // Movie routes
   app.use('/api/movies', moviesRouter);
   // User routes
   // TODO
+  app.use('/api/auth', authRouter);
   app.use('/api/users', usersRouter);
 };
 
